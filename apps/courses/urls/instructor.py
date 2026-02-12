bc3cc8cee361c2c66f9520ae5eebdbedc8a48cd0
Yero-des <yero_zc@hotmail.com>
2026-02-12 12:03:31 -0500

    Vista especial para Content con View Base

--- apps/courses/urls/instructor.py
+++ apps/courses/urls/instructor.py
@@ -5,10 +5,25 @@ app_name = "instructor"
 
 urlpatterns = [
     # Courses URLs
-    path('courses/', instructor.CourseListView.as_view(), name='course_list'),
-    path('course/create/', instructor.CourseCreateView.as_view(), name="course_create"),
-    path('course/<str:slug>/edit/', instructor.CourseUpdateView.as_view(), name="course_edit"), 
-    path('course/<str:slug>/delete/', instructor.CourseDeleteView.as_view(), name="course_delete"),
+    path('courses/', 
+         instructor.CourseListView.as_view(), name='course_list'),
+    path('course/create/', 
+         instructor.CourseCreateView.as_view(), name="course_create"),
+    path('course/<str:slug>/edit/',
+        instructor.CourseUpdateView.as_view(), name="course_edit"), 
+    path('course/<str:slug>/delete/', 
+        instructor.CourseDeleteView.as_view(), name="course_delete"),
     # Modules URLs    
-    path('course/<str:slug>/modules/', instructor.ModuleListView.as_view(), name="module_list"),
+    path('course/<str:slug>/modules/', 
+        instructor.ModuleListView.as_view(), name="module_list"),
+    path('course/<str:slug>/modules/create', 
+        instructor.ModuleCreateView.as_view(), name="module_create"),
+    path('modules/<int:pk>/edit', 
+        instructor.ModuleUpdateView.as_view(), name="module_edit"),
+    path('module/<int:pk>/delete/',
+        instructor.ModuleDeleteView.as_view(), name="module_delete"),
+    # Content URLs
+    path('module/<int:module_id>/contents',
+        instructor.ContentListView.as_view(), name="content_list"),
+    
 ]
--- apps/courses/views/instructor.py
+++ apps/courses/views/instructor.py
@@ -1,14 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from ..models import Course, Module
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from ..models import Course, Module, Content, Text, Image, File, Video
+from django.contrib.contenttypes.models import ContentType
 from django.urls import reverse, reverse_lazy
+from django.forms.models import modelform_factory
+from django.http import HttpResponseForbidden
+
+CONTENT_MODELS = {
+    'text': Text,
+    'video': Video,
+    'image': Image,
+    'video': Video
+}
 
 class InstructorRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
     
     def test_func(self):
         return self.request.user.is_instructor
-
 
 # Course views    
     
@@ -68,7 +77,7 @@ class ModuleListView(InstructorRequiredMixin, ListView):
     def get_queryset(self):
         self.course = get_object_or_404(
             Course, slug=self.kwargs['slug'], owner=self.request.user)
-        return self.course.modules.all()
+        return self.course.modules.all().order_by('order')
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -76,5 +85,123 @@ class ModuleListView(InstructorRequiredMixin, ListView):
         return context
     
     
+class ModuleCreateView(InstructorRequiredMixin, CreateView):
+    model = Module
+    fields = ['title', 'description']
+    template_name = 'instructor/module_form.html'
     
+    def form_valid(self, form):
+        course = get_object_or_404(
+            Course, slug=self.kwargs['slug'], owner=self.request.user)
+        form.instance.course = course
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse_lazy('instructor:module_list', kwargs={
+            'slug': self.object.course.slug
+        })
+
+
+class ModuleUpdateView(InstructorRequiredMixin, UpdateView):
+    model = Module
+    fields = ['title', 'description']
+    template_name = 'instructor/module_form.html'
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(course__owner=self.request.user)
+    
+    def get_success_url(self):
+        return reverse_lazy('instructor:module_list', kwargs={
+            'slug': self.object.course.slug
+        })
+
+
+class ModuleDeleteView(InstructorRequiredMixin, DeleteView):
+    model = Module
+    template_name = 'instructor/module_confirm_delete.html'
+    context_object_name = "module"
+    
+    def get_queryset(self):
+        return super().get_queryset().filter(course__owner=self.request.user)
+    
+    def get_success_url(self):
+        return reverse_lazy('instructor:module_list', kwargs={
+            'slug': self.object.course.slug # El object no se borra asi que aun se puede pasar normalmente
+        })
+        
+
+# Content
+class ContentListView(InstructorRequiredMixin, ListView):
+    model = Content
+    template_name = 'instructor/content_list.html'
+    context_object_name = 'contents'
+    
+    def get_queryset(self):
+        self.module = get_object_or_404(Module, id=self.kwargs['module_id'], course__owner=self.request.user)
+        return self.module.contents.all().select_related('content_type').order_by('order')
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["module"] = self.module
+        return context
+    
+
+class ContentCreateUpdateView(InstructorRequiredMixin, View):
+    template_name = 'instructor/content_form.html'
+    
+    def get_model(self, model_name):
+        return CONTENT_MODELS.get(model_name, None)
+    
+    def get_form(self, model, *args, **kwargs):
+        Form = modelform_factory(
+            model, exclude=['owner', 'created_at', 'updated_at'])
+        return Form(*args, **kwargs)
+        
+    def dispatch(self, request, module_id=None, model_name=None, id=None, *args, **kwargs):
+        self.module = get_object_or_404(
+            Module, id=module_id, course__owner=request.user)
+        self.model = self.get_model(model_name)
+        self.obj = None
+        
+        if id:
+            try:
+                content = Content.objects.select_related('content_type').get(
+                    object_id = id,
+                    content_type=ContentType.objects.get_for_models(self.model),
+                    module=self.module,                    
+                )
+                self.obj = content.item
+            except Content.DoesNotExist:
+                return HttpResponseForbidden("No tienes permiso o tipo invalido")            
+        
+        return super().dispatch(request, module_id=None, model_name=None, id=None, *args, **kwargs)
+    
+    
+    def get(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj)
+        return render(request, self.template_name, {
+            'form': form,
+            'content': self.obj
+        })
+        
+    def post(self, request, module_id, model_name, id=None):
+        form = self.get_form(self.model, instance=self.obj, 
+                             data=request.POST, files=request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.owner = request.user
+            obj.save()
+            
+            if not id:
+                Content.objects.create(module=self.module, item=obj)
+            
+            return reverse_lazy('instructor:content_list', kwargs={
+                'module_id': self.module.id
+            })    
+            
+        return render(request, self.template_name, {
+            'form': form,
+            'content': self.obj
+        })
+                
     
